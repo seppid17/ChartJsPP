bf6efc74d0dd94071e85b65402ceb531975b35d2
thevindu-w <thevinduwijesekera99@gmail.com>
2022-09-21 18:21:28 +0530

    drag and drop csv file parse csv file draw chart from data

--- public/scripts/chart.js
+++ public/scripts/chart.js
@@ -5,16 +5,14 @@ window.onload = () => {
     let height = window.innerHeight;
     let width = window.innerWidth;
     let size = Math.min(height, width);
-    chartDiv.style.width = size+'px';
-    chartDiv.style.height = size+'px';
-    canvas.style.width = size+'px';
-    canvas.style.height = size+'px';
+    chartDiv.style.width = size + 'px';
+    chartDiv.style.height = size + 'px';
+    canvas.style.width = size + 'px';
+    canvas.style.height = size + 'px';
 }
 
 Chart.defaults.font.size = 18;
 const chartName = 'Dataset_1'
-const labels = ['A', 'B', 'C', 'D', 'E'];
-const data = [5, 9, 2, 3, 7];
 const colors = [
     'rgba(255, 99, 132, 1)',
     'rgba(255, 159, 64, 1)',
@@ -23,51 +21,66 @@ const colors = [
     'rgba(201, 203, 207, 1)'
 ];
 
-const dataConf = {
-    labels: labels,
-    datasets: [{
-        label: chartName,
-        data: data,
-        backgroundColor: colors,
-        borderColor: colors,
-        borderWidth: 1
-    }]
-};
+let myChart = null;
+const drawChart = (labels, data)=>{
+    var types = document.getElementsByName('charttype');
+    let type = 'bar';
+    for(i = 0; i < types.length; i++) {
+        if(types[i].checked){
+            type = types[i].value;
+        }
+    }
+    const dataConf = {
+        labels: labels,
+        datasets: [{
+            label: chartName,
+            data: data,
+            backgroundColor: colors,
+            borderColor: colors,
+            borderWidth: 1
+        }]
+    };
 
-const config = {
-    type: 'bar',
-    data: dataConf,
-    options: {
-        maintainAspectRatio: false,
-        responsive: true,
-        layout: {
-            autoPadding: false
-        },
-        scales: {
-            y: {
-                beginAtZero: true
+    const config = {
+        type: type,
+        data: dataConf,
+        options: {
+            maintainAspectRatio: false,
+            responsive: true,
+            layout: {
+                autoPadding: false
             },
-            x: {
-                ticks: {
-                    font: { size: 20 }
+            scales: {
+                y: {
+                    beginAtZero: true
+                },
+                x: {
+                    ticks: {
+                        font: { size: 20 }
+                    }
                 }
             }
-        }
-    },
-};
+        },
+    };
 
-const myChart = new Chart(canvas, config);
-
-canvas.onclick = evt => {
-    let points = myChart.getActiveElements();
-    if (points.length) {
-        const firstPoint = points[points.length - 1];
-        colors[firstPoint.index] = '#000';
-        dataConf.datasets[0].backgroundColor = colors;
-        dataConf.datasets[0].borderColor = colors;
-        myChart.update();
+    if (myChart != null){
+        myChart.destroy();
     }
-};
+    myChart = new Chart(canvas, config);
+
+    canvas.onclick = evt => {
+        let points = myChart.getActiveElements();
+        if (points.length) {
+            const firstPoint = points[points.length - 1];
+            colors[firstPoint.index] = '#000';
+            dataConf.datasets[0].backgroundColor = colors;
+            dataConf.datasets[0].borderColor = colors;
+            myChart.update();
+        }
+    };
+}
+// const labels = ['A', 'B', 'C', 'D', 'E'];
+// const data = [5, 9, 2, 3, 7];
 
 chartDiv.onresize = e => {
     let width = chartDiv.style.width;
@@ -80,8 +93,8 @@ window.onresize = e => {
     let height = window.innerHeight;
     let width = window.innerWidth;
     let size = Math.min(height, width);
-    chartDiv.style.width = size+'px';
-    chartDiv.style.height = size+'px';
+    chartDiv.style.width = size + 'px';
+    chartDiv.style.height = size + 'px';
     chartDiv.onresize(e);
 };
 
@@ -95,3 +108,5 @@ document.getElementById('downloadImgBtn').onclick = e => {
     downLinkTmp.click();
     downLinkTmp.remove();
 };
+
+setCallback(drawChart);
