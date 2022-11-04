c26eddf6d81b465adf7c64fab016f2a34f146f29
thevindu-w <thevinduwijesekera99@gmail.com>
2022-11-04 09:33:45 +0530

    replaced hardcoded names, passwords in account tests

--- test/accFunctionChangeName.test.js
+++ test/accFunctionChangeName.test.js
@@ -1,16 +1,20 @@
-const dotenv =  require("dotenv");
+const dotenv = require("dotenv");
 const { json } = require("express");
 dotenv.config({ path: ".env" });
 dotenv.config({ path: "test/test.env" });
 const superagent = require('superagent');
 const user1 = superagent.agent();
-const domain = 'http://localhost:'+process.env.PORT;
+const domain = 'http://localhost:' + process.env.PORT;
 
+let firstName = '', lastName = '';
 
 beforeAll(async () => {
-    let res =  await user1
-    .post(domain+"/login")
-    .send({ email: process.env.EMAIL, password: process.env.PASSWORD })
+    let res = await user1.post(domain + '/login').send({ email: process.env.EMAIL, password: process.env.PASSWORD });
+    try {
+        res = await user1.get(domain + '/isLogged');
+        let resUser = JSON.parse(res.text);
+        ({ firstName, lastName } = resUser);
+    } catch (e) { }
 });
 
 let isJson = jsonstr => {
@@ -27,81 +31,81 @@ let isJson = jsonstr => {
  * ,firstname and lastname.This testcases check those validation process.
  */
 
-test('Test 1: empty password',async () => {
-    let res = await user1.post(domain+"/account/name").send({ firstName:"first" ,lastName:"last" });
-    let text = res.text
+test('Test 1: empty password', async () => {
+    let res = await user1.post(domain + "/account/name").send({ firstName: "first", lastName: "last" });
+    let text = res.text;
     expect(isJson(text)).toBeTruthy();
-    let obj  = JSON.parse(text);
+    let obj = JSON.parse(text);
     expect(obj.success).toBeFalsy();
     expect(obj.reason).toEqual("Password cannot be empty");
 
 });
 
-test('Test 2: empty firstname',async () => {
-    let res = await user1.post(domain+"/account/name").send({ lastName:"last",password:"1234" });
-    let text = res.text
+test('Test 2: empty firstname', async () => {
+    let res = await user1.post(domain + "/account/name").send({ lastName: "last", password: process.env.PASSWORD });
+    let text = res.text;
     expect(isJson(text)).toBeTruthy();
-    let obj  = JSON.parse(text);
+    let obj = JSON.parse(text);
     expect(obj.success).toBeFalsy();
     expect(obj.reason).toEqual("Name cannot be empty");
 
 });
-test('Test 3: empty lastname',async () => {
-    let res = await user1.post(domain+"/account/name").send({ firstName:"firstname",password:"1234" });
-    let text = res.text
+test('Test 3: empty lastname', async () => {
+    let res = await user1.post(domain + "/account/name").send({ firstName: "firstname", password: process.env.PASSWORD });
+    let text = res.text;
     expect(isJson(text)).toBeTruthy();
-    let obj  = JSON.parse(text);
+    let obj = JSON.parse(text);
     expect(obj.success).toBeFalsy();
     expect(obj.reason).toEqual("Name cannot be empty");
 
 });
-test('Test 4: invalid firstname type',async () => {
-    let res = await user1.post(domain+"/account/name").send({firstName:1234, lastName:"last",password:"1234" });
-    let text = res.text
+test('Test 4: invalid firstname type', async () => {
+    let res = await user1.post(domain + "/account/name").send({ firstName: 1234, lastName: "last", password: process.env.PASSWORD });
+    let text = res.text;
     expect(isJson(text)).toBeTruthy();
-    let obj  = JSON.parse(text);
+    let obj = JSON.parse(text);
     expect(obj.success).toBeFalsy();
     expect(obj.reason).toEqual("Invalid name format");
 
 });
 
-test('Test 5: invalid lastname type',async () => {
-    let res = await user1.post(domain+"/account/name").send({firstName:"firstname", lastName:12345,password:"1234" });
-    let text = res.text
+test('Test 5: invalid lastname type', async () => {
+    let res = await user1.post(domain + "/account/name").send({ firstName: "firstname", lastName: 12345, password: process.env.PASSWORD });
+    let text = res.text;
     expect(isJson(text)).toBeTruthy();
-    let obj  = JSON.parse(text);
+    let obj = JSON.parse(text);
     expect(obj.success).toBeFalsy();
     expect(obj.reason).toEqual("Invalid name format");
 
 });
 
-test('Test 6: invalid password type',async () => {
-    let res = await user1.post(domain+"/account/name").send({firstName:"firstname", lastName:"lastname",password:1234 });
-    let text = res.text
+test('Test 6: invalid password type', async () => {
+    let res = await user1.post(domain + "/account/name").send({ firstName: "firstname", lastName: "lastname", password: 1234 });
+    let text = res.text;
     expect(isJson(text)).toBeTruthy();
-    let obj  = JSON.parse(text);
+    let obj = JSON.parse(text);
     expect(obj.success).toBeFalsy();
     expect(obj.reason).toEqual("Invalid password format");
 
 });
 
-test('Test 7: invalid password matching',async () => {
-    let res = await user1.post(domain+"/account/name").send({firstName:"harshana", lastName:"eranda",password:"password2" });
+test('Test 7: invalid password matching', async () => {
+    let res = await user1.post(domain + "/account/name").send({ firstName: "firstname", lastName: "lastname", password: "Notpassword" });
     let text = res.text;
     expect(isJson(text)).toBeTruthy();
-    let obj  = JSON.parse(text);
+    let obj = JSON.parse(text);
     expect(obj.success).toBeFalsy();
     expect(obj.reason).toEqual("Incorrect password");
 
 });
 
-test('Test 8: valid password',async () => {
-    let res = await user1.post(domain+"/account/name").send({firstName:"harshana", lastName:"eranda",password:"password" });
-    let text = res.text
+test('Test 8: valid password', async () => {
+    let res = await user1.post(domain + "/account/name").send({ firstName: firstName, lastName: lastName, password: process.env.PASSWORD });
+    let text = res.text;
     expect(isJson(text)).toBeTruthy();
-    let obj  = JSON.parse(text);
+    let obj = JSON.parse(text);
     expect(obj.success).toBeTruthy();
-    
+
 
 });
 
--- test/accFunctionChangePassword.test.js
+++ test/accFunctionChangePassword.test.js
@@ -1,16 +1,14 @@
-const dotenv =  require("dotenv");
+const dotenv = require("dotenv");
 const { json } = require("express");
 dotenv.config({ path: ".env" });
 dotenv.config({ path: "test/test.env" });
 const superagent = require('superagent');
 const user1 = superagent.agent();
-const domain = 'http://localhost:'+process.env.PORT;
+const domain = 'http://localhost:' + process.env.PORT;
 
 
 beforeAll(async () => {
-    let res =  await user1
-    .post(domain+"/login")
-    .send({ email: process.env.EMAIL, password: process.env.PASSWORD })
+    let res = await user1.post(domain + "/login").send({ email: process.env.EMAIL, password: process.env.PASSWORD });
 });
 
 let isJson = jsonstr => {
@@ -28,56 +26,57 @@ let isJson = jsonstr => {
  * and new password.This testcases check those validation process.
  */
 
-test("Test 1: current password empty",async () =>{
-    let res = await user1.post(domain+"/account/password").send({ newPassword:"newpassword"});
-    let text = res.text
+test("Test 1: current password empty", async () => {
+    let res = await user1.post(domain + "/account/password").send({ newPassword: "newpassword" });
+    let text = res.text;
     expect(isJson(text)).toBeTruthy();
-    let obj  = JSON.parse(text);
+    let obj = JSON.parse(text);
     expect(obj.success).toBeFalsy();
     expect(obj.reason).toEqual("Password cannot be empty");
 });
 
-test("Test 2: current password validation",async () =>{
-    let res = await user1.post(domain+"/account/password").send({curPassword:1234, newPassword:"password"});
-    let text = res.text
+test("Test 2: current password validation", async () => {
+    let res = await user1.post(domain + "/account/password").send({ curPassword: 1234, newPassword: "password" });
+    let text = res.text;
     expect(isJson(text)).toBeTruthy();
-    let obj  = JSON.parse(text);
+    let obj = JSON.parse(text);
     expect(obj.success).toBeFalsy();
     expect(obj.reason).toEqual("Invalid password format");
 });
 
-test("Test 3: new password empty",async () =>{
-    let res =await user1.post(domain+"/account/password").send({curPassword:"password"});
-    let text =res.text;
+test("Test 3: new password empty", async () => {
+    let res = await user1.post(domain + "/account/password").send({ curPassword: process.env.PASSWORD });
+    let text = res.text;
     expect(isJson(text)).toBeTruthy();
-    let obj  = JSON.parse(text);
+    let obj = JSON.parse(text);
     expect(obj.success).toBeFalsy();
     expect(obj.reason).toEqual("Password cannot be empty");
 });
 
-test("Test 4: new password validation",async () =>{
-    let res = await user1.post(domain+"/account/password").send({curPassword:"password", newPassword:1234});
-    let text = res.text
+test("Test 4: new password validation", async () => {
+    let res = await user1.post(domain + "/account/password").send({ curPassword: process.env.PASSWORD, newPassword: 1234 });
+    let text = res.text;
     expect(isJson(text)).toBeTruthy();
-    let obj  = JSON.parse(text);
+    let obj = JSON.parse(text);
     expect(obj.success).toBeFalsy();
     expect(obj.reason).toEqual("Invalid password format");
 });
 
-test("Test 5: Incorrect password",async () =>{
-    let res = await user1.post(domain+"/account/password").send({curPassword:"Notpassword", newPassword:"password"});
-    let text = res.text
+test("Test 5: Incorrect password", async () => {
+    let res = await user1.post(domain + "/account/password").send({ curPassword: "Notpassword", newPassword: "password" });
+    let text = res.text;
     expect(isJson(text)).toBeTruthy();
-    let obj  = JSON.parse(text);
+    let obj = JSON.parse(text);
     expect(obj.success).toBeFalsy();
     expect(obj.reason).toEqual("Incorrect password");
 });
 
-test("Test 6: change password",async () =>{
-    let res = await user1.post(domain+"/account/password").send({curPassword:"password", newPassword:"password"});
-    let text = res.text
+test("Test 6: change password", async () => {
+    // updates the password with the same password to prevent password change after test
+    let res = await user1.post(domain + "/account/password").send({ curPassword: process.env.PASSWORD, newPassword: process.env.PASSWORD });
+    let text = res.text;
     expect(isJson(text)).toBeTruthy();
-    let obj  = JSON.parse(text);
+    let obj = JSON.parse(text);
     expect(obj.success).toBeTruthy();
-    
+
 });
