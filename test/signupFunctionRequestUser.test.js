3982e8274031e22a883ccb372fa1cc9d6ae1af0a
thevindu-w <thevinduwijesekera99@gmail.com>
2022-11-12 23:42:10 +0530

    separated email reading funcionality to a dedicated function

--- test/signupFunctionRequestUser.test.js
+++ test/signupFunctionRequestUser.test.js
@@ -7,7 +7,7 @@ const user1 = superagent.agent();
 const domain = 'http://localhost:' + process.env.PORT;
 const validPassword = 'password';
 
-let isJson = jsonstr => {
+const isJson = jsonstr => {
     try {
         let obj = JSON.parse(jsonstr);
         return true;
@@ -24,6 +24,62 @@ const imap = new Imap({
     tls: true,
     tlsOptions: { rejectUnauthorized: false }
 });
+
+const receiveEmails = (callback) => {
+    return new Promise(function (resolve, reject) {
+        imap.once('ready', () => {
+            try {
+                imap.openBox('INBOX', false, (err, box) => {
+                    if (err) throw err;
+                    imap.search(['UNSEEN', ['SINCE', new Date()]], (err, results) => {
+                        if (err) throw err;
+                        if (!results || !results.length) {
+                            imap.end();
+                            return;
+                        }
+                        let f = imap.fetch(results, {
+                            bodies: ['HEADER.FIELDS (FROM)', 'TEXT'],
+                            struct: true
+                        });
+                        imap.setFlags(results, ['\\Seen'], function (err) {
+                            if (err) {
+                                console.log(err);
+                            }
+                        });
+                        f.on('message', (msg, seqno) => {
+                            msg.on('body', (stream, info) => {
+                                let buffer = '', count = 0;
+                                stream.on('data', (chunk) => {
+                                    count += chunk.length;
+                                    buffer += chunk.toString('utf8');
+                                });
+                                stream.once('end', async () => {
+                                    buffer = buffer.replaceAll('=\n', '').replaceAll('=\r', '').replaceAll('\n', '').replaceAll('\r', '');
+                                    if (await callback(buffer)){
+                                        resolve();
+                                    }
+                                });
+                            });
+                        });
+                        f.once('error', (err) => {
+                            console.log('Fetch error: ' + err);
+                        });
+                        f.once('end', () => {
+                            imap.end();
+                        });
+                    });
+                });
+            } catch (e) {
+            }
+        });
+
+        imap.once('error', (err) => {
+            console.log(err);
+        });
+
+        imap.connect();
+    });
+}
 
 test('Test 1: empty email', async () => {
     let res = await user1.post(domain + '/signup').send({ password: validPassword, firstName: "dumyfirstname", lastName: "dumylastname" });
@@ -116,77 +172,30 @@ test('Test 10: signup to new account', async () => {
 
 test('Test 11: activate new account', async () => {
     let done = false;
-    await new Promise(function (resolve, reject) {
-        imap.once('ready', () => {
-            try {
-                imap.openBox('INBOX', false, (err, box) => {
-                    if (err) throw err;
-                    imap.search(['UNSEEN', ['SINCE', new Date()]], (err, results) => {
-                        if (err) throw err;
-                        if (!results || !results.length) {
-                            imap.end();
-                            return;
-                        }
-                        let f = imap.fetch(results, {
-                            bodies: ['HEADER.FIELDS (FROM)', 'TEXT'],
-                            struct: true
-                        });
-                        imap.setFlags(results, ['\\Seen'], function (err) {
-                            if (err) {
-                                console.log(err);
-                            }
-                        });
-                        f.on('message', (msg, seqno) => {
-                            msg.on('body', (stream, info) => {
-                                let buffer = '', count = 0;
-                                stream.on('data', (chunk) => {
-                                    count += chunk.length;
-                                    buffer += chunk.toString('utf8');
-                                });
-                                stream.once('end', async () => {
-                                    buffer = buffer.replaceAll('=\n', '').replaceAll('=\r', '').replaceAll('\n', '').replaceAll('\r', '');
-                                    let re = new RegExp(domain + '/activate/' + process.env.GMAIL_SENDER + '/[0-9A-Za-z]{1,32}');
-                                    let match = re.exec(buffer);
-                                    if (match) {
-                                        let activateLink = match[0];
-                                        {
-                                            let res = await user1.post(activateLink).send({ password: 'incorrectPwd' });
-                                            let text = res.text;
-                                            expect(isJson(text)).toBeTruthy();
-                                            let obj = JSON.parse(text);
-                                            expect(obj.success).toBeFalsy();
-                                            expect(obj.reason).toEqual('Incorrect password');
-                                        }
-                                        {
-                                            let res = await user1.post(activateLink).send({ password: validPassword });
-                                            let text = res.text;
-                                            expect(isJson(text)).toBeTruthy();
-                                            let obj = JSON.parse(text);
-                                            expect(obj.success).toBeTruthy();
-                                            done = true;
-                                            resolve();
-                                        }
-                                    }
-                                });
-                            });
-                        });
-                        f.once('error', (err) => {
-                            console.log('Fetch error: ' + err);
-                        });
-                        f.once('end', () => {
-                            imap.end();
-                        });
-                    });
-                });
-            } catch (e) {
+    await receiveEmails(async buffer => {
+        let re = new RegExp(domain + '/activate/' + process.env.GMAIL_SENDER + '/[0-9A-Za-z]{1,32}');
+        let match = re.exec(buffer);
+        if (match) {
+            let activateLink = match[0];
+            {
+                let res = await user1.post(activateLink).send({ password: 'incorrectPwd' });
+                let text = res.text;
+                expect(isJson(text)).toBeTruthy();
+                let obj = JSON.parse(text);
+                expect(obj.success).toBeFalsy();
+                expect(obj.reason).toEqual('Incorrect password');
             }
-        });
-
-        imap.once('error', (err) => {
-            console.log(err);
-        });
-
-        imap.connect();
+            {
+                let res = await user1.post(activateLink).send({ password: validPassword });
+                let text = res.text;
+                expect(isJson(text)).toBeTruthy();
+                let obj = JSON.parse(text);
+                expect(obj.success).toBeTruthy();
+                done = true;
+                return true;
+            }
+        }
+        return false;
     });
     expect(done).toBeTruthy();
 }, 20000);
